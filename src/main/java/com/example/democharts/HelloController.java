482b008562ade334232d7d44c4d5c80be2848d33
KoshkinVitaliy <kosh98vitaliy@outlook.com>
2023-11-30 13:56:33 +0300

    version2.0

--- src/main/java/com/example/democharts/HelloController.java
+++ src/main/java/com/example/democharts/HelloController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.chart.Axis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
@@ -11,6 +12,7 @@ import javafx.stage.Stage;
 
 
 public class HelloController {
+
     @FXML
     private LineChart graphChart;
 
@@ -18,21 +20,25 @@ public class HelloController {
     @FXML
     protected void createChartBtnClick(ActionEvent actionEvent) {
         NumberAxis x = new NumberAxis(1960, 2020, 10);
+
+
         x.setLabel("Years");
 
         NumberAxis y = new NumberAxis(0, 350, 50);
         y.setLabel("No.of schools");
 
+
         graphChart = new LineChart<>(x,y);
+
         XYChart.Series series = new XYChart.Series();
         series.setName("No of schools in an year");
 
-        series.getData().add(new XYChart.Data(1970, 15));
+        series.getData().add(new XYChart.Data<>(1970, 15));
         series.getData().add(new XYChart.Data(1980, 30));
-        series.getData().add(new XYChart.Data(1990, 60));
-        series.getData().add(new XYChart.Data(2000, 120));
-        series.getData().add(new XYChart.Data(2013, 240));
-        series.getData().add(new XYChart.Data(2014, 300));
+        series.getData().add(new XYChart.Data<>(1990, 60));
+        series.getData().add(new XYChart.Data<>(2000, 120));
+        series.getData().add(new XYChart.Data<>(2013, 240));
+        series.getData().add(new XYChart.Data<>(2014, 300));
 
         //Setting the data to Line chart
         graphChart.getData().add(series);
